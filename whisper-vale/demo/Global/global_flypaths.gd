3a8d734dbe3668c6523cf35133f87845191a3b72
Givr <lightpower@hotmail.fr>
2025-06-05 01:42:57 +0200

    working on getting nearest fly point

--- whisper-vale/demo/Global/global_flypaths.gd
+++ whisper-vale/demo/Global/global_flypaths.gd
@@ -1,7 +1,21 @@
 extends Node
 
-@onready var fly_points = get_tree().get_nodes_in_group("fly_points") as Array[fly_point]
-@onready var fly_paths = get_tree().get_nodes_in_group("fly_paths") as Array[fly_path]
+@onready var fly_points:Array[fly_point] = get_tree().get_nodes_in_group("fly_points") as Array[fly_point]
+@onready var fly_paths:Array[fly_path] = get_tree().get_nodes_in_group("fly_paths") as Array[fly_path]
+
+func get_nearest_fly_point(position:Vector3)-> fly_point:
+	var nearest_point = {"point": null, "distance": Vector3.ZERO}
+	for point in fly_points:
+		var distance:float = position.distance_to(point.position)
+		if nearest_point.point == null:
+			nearest_point.point = point
+			nearest_point.distance = distance
+		
+		if distance < nearest_point.distance:
+			nearest_point.point = point
+			nearest_point.distance = distance
+	return nearest_point.point
+	
 
 func _ready():
 	
