74377279e8656f520b2eb3c5300e80eb10e52c02
p.houot <>
2025-06-04 19:57:51 +0200

    fly handled with path

--- whisper-vale/demo/flypoint/fly_path.gd
+++ whisper-vale/demo/flypoint/fly_path.gd
@@ -7,13 +7,30 @@ class_name fly_path extends Path3D
 @onready var path_name:StringName = fly_point_1.flyname + "-" + fly_point_2.flyname
 @onready var path_follow: PathFollow3D = $PathFollow3D
 
-var is_flying = false
-var flying_body = null
-
+var is_flying := false
+var flying_body:Player = null
+var invert_fly_direction := false
+	
 func _process(delta: float) -> void:
 	if is_flying:
-		if path_follow.progress_ratio <= 0:
-			is_flying = false
-			path_follow.progress_ratio = 0
-		if is_flying:
-			path_follow.progress -= delta * fly_speed
+		update_fly(delta)
+			
+func start_fly (body:Player):
+	is_flying = true
+	flying_body = body
+	flying_body.can_move = false
+	flying_body.reparent(path_follow)
+	flying_body.position = Vector3(0,0,0)
+	
+func update_fly(delta:float):
+	if path_follow.progress_ratio <= 0:
+		stop_fly()
+	if is_flying:
+		path_follow.progress -= delta * fly_speed
+
+func stop_fly():
+	is_flying = false
+	path_follow.progress = 0
+	flying_body.can_move = true
+	flying_body.reparent(get_tree().root)
+	flying_body = null
--- whisper-vale/demo/flypoint/fly_point.gd
+++ whisper-vale/demo/flypoint/fly_point.gd
@@ -13,18 +13,8 @@ var landing_position = Vector3(0,0,0)
 func _ready() -> void:
 	setup_label()
 	
-func _process(delta: float) -> void:
-	if !is_flying:
-		return
-		
-	if flying_progress >= 1:
-		stop_fly()
-		return
-		
-	update_fly(delta)
-	
 func setup_label():
-	var fly_label = Label3D.new()
+	var fly_label := Label3D.new()
 	fly_label.font_size = 64
 	fly_label.text = flyname
 	add_child(fly_label)
@@ -34,8 +24,8 @@ func get_first_fly_point() -> fly_point:
 	return Flypaths.fly_points.filter(func(fly:fly_point): return fly.flyname != flyname)[0]
 	
 func find_road(next_fly_name:StringName)-> fly_path:
-	var current_point = self.flyname
-	var next_point = next_fly_name
+	var current_point := self.flyname
+	var next_point := next_fly_name
 	var path_road: fly_path = Flypaths.fly_paths.filter(func(path:fly_path):
 		return (( path.fly_point_1.flyname == current_point ||
 		 	path.fly_point_2.flyname == current_point ) &&
@@ -45,32 +35,12 @@ func find_road(next_fly_name:StringName)-> fly_path:
 	
 	return path_road
 	
-func start_fly(body: Node3D, new_position: Vector3):
-	is_flying = true
-	flying_body = body
-	starting_position = body.position
-	landing_position = new_position
-	
-func update_fly(delta:float):
-	flying_progress += delta
-	flying_body.position = starting_position.lerp(landing_position, flying_progress)
-	
-func stop_fly():
-	is_flying = false
-	flying_progress = 0
-	starting_position = Vector3(0,0,0)
-	landing_position = Vector3(0,0,0)
-	flying_body = null
-	
 func _on_interact_area_body_entered(player: Player) -> void:
-	var nextFly = get_first_fly_point()
-	var fly_road = find_road(nextFly.flyname)
+	var nextFly := get_first_fly_point()
+	var fly_road := find_road(nextFly.flyname)
 	if not fly_road.is_flying:
 		print("Fly from ", [self.flyname], " to ", [nextFly.flyname], " via ", [fly_road.path_name], " road")
-		player.can_move = false
-		fly_road.is_flying = true
-		player.reparent(fly_road.path_follow)
-		player.position = Vector3(0,0,0)
-		#start_fly(body, nextFly.position)
+		fly_road.start_fly(player)
+		
 
 #func _on_interact_area_body_exited(body: Node3D) -> void:
