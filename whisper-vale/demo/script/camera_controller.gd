4a0ec1aa40e9d0c236e339181deeabbd3e60e08b
p.houot <>
2025-12-05 15:57:28 +0100

    rename + reset height for camera

--- whisper-vale/demo/script/camera_controller.gd
+++ whisper-vale/demo/script/camera_controller.gd
@@ -1,12 +1,13 @@
 class_name CameraController extends Node3D
 
-@export var player: CharacterBody3D
+@export var player: Player
 @export var distance_max: float = 3
 
 @export var travel_speed: float = 7
 
 @export var target_lookat_offset: float = 2
-@onready var Camera: Camera3D = $Camera3D
+@export var camera_height_offset: float = 1.5
+#@onready var Camera: Camera3D = $Camera3D
 
 @export var rotation_speed: float = 0.1
 
@@ -19,7 +20,8 @@ func _physics_process(delta: float) -> void:
 	var velocity: Vector2 = Input.get_last_mouse_velocity()
 	mouse_rotate_camera(velocity * rotation_speed * delta / 100)
 	self.transform = self.transform.looking_at(player.global_position + (Vector3.UP * target_lookat_offset))
-	player_cast()
+	obstacle_player_cast()
+	auto_reset_height(delta)
 
 func mouse_rotate_camera(move):
 	const ratio_compensation: float = 1.5
@@ -27,24 +29,38 @@ func mouse_rotate_camera(move):
 
 func translate_controller(delta: float):
 	var distance_to_player: float = player.global_position.distance_to(self.global_position)
+	
 	if (distance_to_player > distance_max):
 		var moving_speed = - delta * travel_speed * max(abs(distance_to_player - distance_max), 0)
 		self.translate_object_local(Vector3(0, 0, moving_speed))
 	
-func player_cast():
-	var space_state := get_world_3d().direct_space_state
-	# use global coordinates, not local to node
+func obstacle_player_cast():
 	var exclude_array = [player.get_rid(), RID(self)]
+	
 	var req = PhysicsRayQueryParameters3D.new()
 	req.from = player.global_position
 	req.to = self.global_position
 	req.exclude = exclude_array
 	
-	var result := space_state.intersect_ray(req)
+	var result := get_world_3d().direct_space_state.intersect_ray(req)
 	
 	if (result.size() > 0):
 		var collision = result["collider"]
 		if ( collision ):
 			self.position = result["position"]
-			var safety_margin := 0.1
+			var safety_margin := 0.15
 			self.translate_object_local(Vector3(0, safety_margin, 0))
+			
+func auto_reset_height(delta):
+	var exclude_array = [player.get_rid(), RID(self)]
+	
+	var req = PhysicsRayQueryParameters3D.new()
+	req.from = self.position
+	req.to = self.position + (Vector3.DOWN * camera_height_offset)
+	req.exclude = exclude_array
+	var result := get_world_3d().direct_space_state.intersect_ray(req)
+	
+	if (result.size() > 0):
+		var collision = result["collider"]
+		if ( collision ):
+			self.translate(Vector3.UP * delta * rotation_speed)
