ee269a7c7dd852863838ae9a82ff4cb5f55548a0
Givr <lightpower@hotmail.fr>
2025-12-27 17:50:49 +0100

    fixed QuestStep instantiation -> quest  tocollect now work again

--- whisper-vale/demo/script/Quest/quest_data.gd
+++ whisper-vale/demo/script/Quest/quest_data.gd
@@ -7,6 +7,14 @@ var quest_steps : Array[QuestStepData] = []
 func populate(resource:Quest):
 	tag = resource.tag
 	for step_resource in resource.quest_steps:
-		var quest_step = QuestStepData.new()
+		var quest_step = null
+		
+		match step_resource.get_script():
+			QuestStep:
+				quest_step = QuestStepData.new()
+			QuestStepItem:
+				quest_step = QuestStepItemData.new()
+				quest_step.item_to_collect = step_resource.item_to_collect
+		
 		quest_step.tag = step_resource.tag
 		quest_steps.append(quest_step)
