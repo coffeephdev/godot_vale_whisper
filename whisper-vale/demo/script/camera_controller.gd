c97fedc314be0bde26c958e31c63b9ccea312eb0
p.houot <>
2026-01-06 12:20:48 +0100

    way better cam

--- whisper-vale/demo/script/camera_controller.gd
+++ whisper-vale/demo/script/camera_controller.gd
@@ -1,4 +1,4 @@
-class_name CameraController extends RigidBody3D
+class_name CameraController extends Node3D
 
 @export var player: Player
 @export var target_lookat_offset: float = 2
@@ -15,7 +15,7 @@ class_name CameraController extends RigidBody3D
 
 const min_cam_distance = 3
 const max_cam_distance = 6
-@onready var cam_distance = player_distance
+var cam_distance: float
 
 const screen_ratio = Vector2(1, .56)
 
@@ -24,6 +24,7 @@ func _ready() -> void:
 	#self.global_position.y = player.global_position.y + target_lookat_offset
 	SaveLead.add_to_register(self)
 	GameLead.camera = self
+	cam_distance = player_distance
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if (event.is_action("scroll_up")):
@@ -34,26 +35,27 @@ func _unhandled_input(event: InputEvent) -> void:
 	cam_distance = clampf(cam_distance, min_cam_distance, max_cam_distance)
 	
 func _physics_process(delta: float) -> void:
-	translate_controller(delta)
+	var distance_to_player: float = player.global_position.distance_to(self.global_position)
+	
+	translate_controller(delta, distance_to_player)
 	obstacle_player_cast()
-	auto_reset_height(delta)
 	
 	if (!player.CAN_MOVE): return
 	
 	var velocity: Vector2 = Input.get_last_mouse_velocity() * screen_ratio
-	mouse_rotate_camera(velocity * rotation_speed * delta / 100)
+	mouse_rotate_camera(velocity * rotation_speed * distance_to_player * delta / 1000)
 	self.transform = self.transform.looking_at(player.global_position + (Vector3.UP * target_lookat_offset))
 
 func mouse_rotate_camera(move):
 	const ratio_compensation: float = 1.5
 	self.translate_object_local(Vector3(-move.x, move.y * ratio_compensation, 0))
 
-func translate_controller(delta: float):
-	var distance_to_player: float = player.global_position.distance_to(self.global_position)
+func translate_controller(delta: float, distance_to_player: float):
 	
 	if (distance_to_player > cam_distance):
-		var moving_speed = - delta * travel_speed * max(distance_to_player - cam_distance, 0)
-		self.translate_object_local(Vector3(0, 0, moving_speed))
+		pass
+	var moving_speed = delta * travel_speed * (distance_to_player - cam_distance)
+	self.translate_object_local(Vector3(0, 0, -moving_speed))
 	
 func obstacle_player_cast():
 	var exclude_array = [player.get_rid(), RID(self)]
@@ -69,19 +71,5 @@ func obstacle_player_cast():
 		var collision = result["collider"]
 		if ( collision ):
 			self.position = result["position"]
-			var safety_margin := 0.15
+			var safety_margin := 0.55
 			self.translate_object_local(Vector3(0, safety_margin, 0))
-			
-func auto_reset_height(delta):
-	var exclude_array = [player.get_rid(), RID(self)]
-	
-	var req = PhysicsRayQueryParameters3D.new()
-	req.from = self.position
-	req.to = self.position + (Vector3.DOWN * camera_height_offset)
-	req.exclude = exclude_array
-	var result := get_world_3d().direct_space_state.intersect_ray(req)
-	
-	if (result.size() > 0):
-		var collision = result["collider"]
-		if ( collision ):
-			self.translate(Vector3.UP * delta * rotation_speed)
